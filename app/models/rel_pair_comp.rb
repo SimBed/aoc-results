37429bcc9b83df5c7a08465381dd6c3b7218a021
SimBed <dansimbed@gmail.com>
2021-10-03 11:36:05 +0100

    divide by zero correct

--- app/models/rel_pair_comp.rb
+++ app/models/rel_pair_comp.rb
@@ -4,6 +4,7 @@ class RelPairComp < ApplicationRecord
   scope :order_by_date_score, -> { order(comp_id: :desc, score: :desc ) }
 
   def position_pct
+    return 0 if Comp.find(comp_id).nil? || Comp.find(comp_id).zero?
     position.to_f / Comp.find(comp_id).pair_count
   end
 end
