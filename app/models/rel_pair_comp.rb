61dc43e634a4825860767e750edc872bd010e7eb
SimBed <dansimbed@gmail.com>
2021-10-03 11:49:57 +0100

    recorrection of divide by zero

--- app/models/rel_pair_comp.rb
+++ app/models/rel_pair_comp.rb
@@ -4,7 +4,7 @@ class RelPairComp < ApplicationRecord
   scope :order_by_date_score, -> { order(comp_id: :desc, score: :desc ) }
 
   def position_pct
-    return 0 if Comp.find(comp_id).nil? || Comp.find(comp_id).zero?
+    return 0 if Comp.find(comp_id).pair_count.zero?
     position.to_f / Comp.find(comp_id).pair_count
   end
 end
